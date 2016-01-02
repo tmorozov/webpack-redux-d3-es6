f2c03dc9cfd7d4ffea0be7cf8e333e5e58983625
Timur Morozov <radist@desant.biz>
2016-01-02 18:31:08 +0200

    move render to view

--- src/js/view/main.js
+++ src/js/view/main.js
@@ -1,7 +1,43 @@
+import d3 from "d3";
+import topojson from "topojson";
+import geoProjection from "d3-geo-projection";
+geoProjection(d3);
 
-const mainView = (el) => el.append("text")
-    .attr("x", 100)
-    .attr("y", 100)
-    .text("Hello");
+const width = 800;
+const height = 600;
 
-export default mainView;
+let projection = d3.geo.patterson()
+    .scale(153)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+let path = d3.geo.path()
+    .projection(projection);
+
+let graticule = d3.geo.graticule();
+
+let svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+
+function render(world) {
+    svg.insert("path", ".graticule")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path);
+}
+
+export default {
+    render
+};
